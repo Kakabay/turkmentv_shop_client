8941382f0e01ef66a602db730b02a3f231c977dc
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-02-07 18:00:45 +0500

    minor set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        textBlack: '#242429',
+        textLight: '#878799',
+        textDarkt: '#636370',
+        fillLightBgLightContr: '#F5F5FA',
+        fillButtonAccentDefault: '#7A7ACC',
+        fillTableHead: '#E1E1F5',
+        fillTableRow: '#F0F0FA',
+        fillTableRow2: '#F5F5FA',
+        fillTableStrokeTableHead: '#dcdcfa',
+        fillTableStrokeTableRow: '#E6E6FA',
       },
     },
   },
