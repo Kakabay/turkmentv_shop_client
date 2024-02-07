d9a1a19a62f51a27192aae39d4a09b1efcb8da5c
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-02-07 18:00:32 +0500

    lot page

---
+++ app/(root)/lot/[lot_number]/page.tsx
@@ -0,0 +1,15 @@
+import Container from '@/components/shared/container';
+import ShopTable from '@/components/shop/shopTable';
+import React from 'react';
+
+const LotNumber = () => {
+  return (
+    <main className="mt-[100px]">
+      <Container>
+        <ShopTable />
+      </Container>
+    </main>
+  );
+};
+
+export default LotNumber;
